3485f0429063d91011d77cfea7fa489af6c47f5c
unknown <gildenbrand.daniel@gmail.com>
2023-06-03 03:19:09 -0400

    feat: actually write uploaded image to ipfs

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -18,22 +18,38 @@ import { create } from "ipfs-http-client";
 export async function uploadToIPFS(filePath: string): Promise<string> {
   try {
     // connect to the IPFS API server
-    const ipfs = create();
-    console.log(
-      "************************************created ipfs client",
-      filePath
-    );
-    console.log("************************************is online2");
+    const ipfs = create({
+      host: "127.0.0.1",
+      port: 5001,
+      protocol: "http",
+    });
+    try {
+      // Ping the IPFS node
+      await ipfs.id();
+      console.log("******************Connected to IPFS.");
+    } catch (error) {
+      console.error("****************Failed to connect to IPFS:", error);
+      throw error;
+    }
     // read the file
     const file = await (await fetch(filePath)).blob();
 
     console.log("fetched file", filePath);
 
     // convert the file to a format that can be uploaded to IPFS
-    const fileForIpfs = { path: crypto.randomUUID(), content: file };
+    const newId = crypto.randomUUID();
+    const extension = filePath.split(".")[-1];
+    const fileName = "/" + newId + "." + (extension ?? "jpg");
+    const fileForIpfs = {
+      path: fileName,
+      content: file,
+    };
 
     // upload the file
-    const result = await ipfs.files.write("./", file);
+    const result = await ipfs.files.write(fileName, file, {
+      create: true,
+    });
+    //const result = await ipfs.add(fileForIpfs);
     console.log("added file", result);
 
     // return the CID
