dcac685df09da237e767e714c9e18a48d83c527b
unknown <gildenbrand.daniel@gmail.com>
2023-06-03 23:16:04 -0400

    feat: add wallet connect using rainbow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,37 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig, mainnet } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID ?? "" }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "OnlyChains",
+  projectId: "YOUR_PROJECT_ID",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={[mainnet]}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 };
 
 export default api.withTRPC(MyApp);
--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -4,6 +4,7 @@ import type { OurFileRouter } from "~/server/uploadthing";
 // You need to import our styles for the button to look right. Best to import in the root /_app.tsx but this is fine
 import "@uploadthing/react/styles.css";
 import Image from "next/image";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   const [files, setFiles] = useState<
@@ -12,6 +13,15 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section
+        data-testid="header"
+        className="absolute top-0 flex flex-row justify-between"
+      >
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          OpenChains
+        </h1>
+        <ConnectButton />
+      </section>
       {files?.map((file) => (
         <Image key={file.fileKey} src={file.fileUrl} alt="" fill />
       ))}
