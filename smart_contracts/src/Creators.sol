713db2849de2953cca69f9bbeb68bb6f7a0c886a
crypdough.eth <106392730+crypdoughdoteth@users.noreply.github.com>
2023-06-17 22:14:52 -0400

    Update Creators.sol

--- smart_contracts/src/Creators.sol
+++ smart_contracts/src/Creators.sol
@@ -4,10 +4,11 @@ import "./CreatorContent.sol";
 
 contract Creators {
 
-    mapping (address => CreatorContent) public creators;
+    mapping (address => address) public creators;
     
     function newCreator(string calldata uri, address currency) public {
+        require(creators[msg.sender] == address(0), "Already Exists");
         CreatorContent addy = new CreatorContent(uri, msg.sender, currency);
-        creators[msg.sender] = addy;
+        creators[msg.sender] = address(addy);
     }
 }
